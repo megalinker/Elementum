9f5aa0796c2c37932fc0b566b87241070e805270
capuzr <capuzr@gmail.com>
2024-10-17 11:49:04 -0400

    chore: add BOOM quest images

---
+++ juno.config.js
@@ -0,0 +1,9 @@
+import {defineConfig} from '@junobuild/config';
+
+/** @type {import('@junobuild/config').JunoConfig} */
+export default defineConfig({
+  satellite: {
+    id: 'lmtej-zaaaa-aaaal-adyvq-cai',
+    source: 'dist/e_website_v4'
+  }
+});
